0e8f785e0361cd3826f0fb43c777fc51526210df
Your Name <email@example.com>
2021-09-08 10:58:41 +0800

    version1.2-- 增添二维码扫码辅助签到功能

--- WebContent/js/web_js/sign_up.js
+++ WebContent/js/web_js/sign_up.js
@@ -1,13 +1,13 @@
 function rigister(){
-	var user_name = document.getElementById("user_name").value;
-	var user_password = document.getElementById("user_password").value;
-	var user_ID = document.getElementById("user_ID").value;
+	var user_name = document.getElementById("user_name").value;//获取id为user_name的value
+	var user_password = document.getElementById("user_password").value;//获取id为uuser_password的value
+	var user_ID = document.getElementById("user_ID").value;//获取id为user_ID的value
 	var openID = 0;
 
-	var url = "http://daipeng.nat300.top/sign_up_servlet";
+	var url = "http://daipeng.nat300.top/sign_up_servlet";//后端的java servlet（即前端传值）
 
     var index = location.href;
-    var open_ID =index.substr(index.indexOf('=') + 1) ;
+    var open_ID =index.substr(index.indexOf('=') + 1) ;//分离得到open_ID
 
 	if(user_name == "" || user_name == null || user_password == "" || user_password == null || user_ID == "" || user_ID == null){
 		alert("请填满所有空文本！！！");
